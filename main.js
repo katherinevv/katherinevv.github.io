cbb96ec8e9d44fa5727ecd26f1462b10f81d24fe
katherinevv <katherine.vanessa12.4.2005@gmail.com>
2024-10-20 00:51:31 +0700

    initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+// Position
+var typed = new Typed(".text", {
+    strings: ["UI/UX Designer", "Front-end Developer", "Back-end Developer", "Web Developer", "Mobile App Developer"],
+    typeSpeed: 100,
+    backSpeed: 200,
+    backDelay: 1000,
+    loop: true
+});
